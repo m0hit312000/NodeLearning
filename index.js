ed7f964f054d8925c7b6b182111041068744c96b
Mohit <mm8085574989@gmail.com>
2020-08-09 18:03:15 +0530

    simple Node Example

---
+++ index.js
@@ -0,0 +1,22 @@
+var rect = {
+    perimemter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+
+function solveReact(l,b)
+{
+    console.log("Solving for rectangle with l = " + l + " and b = " + b);
+    if(l <= 0||b <= 0)
+    {
+        console.log("Rectangle dimensions should be greater than 0 l = " + l + " and b = " + b );
+    }
+    else
+    {
+        console.log("The area of the rectangle is" + rect.area(l,b));
+        console.log("The perimeter of the rectangle is" + rect.perimemter(l,b));
+    }
+}
+solveReact(2,4);
+solveReact(5,10);
+solveReact(3,9);
+solveReact(22,30);
